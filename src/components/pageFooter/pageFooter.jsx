05c4fd883f735c1688229d1922e249a453a08deb
vanemercader <vanessa.mercader@gmail.com>
2026-05-20 12:37:42 -0300

    fix del pageFooter

--- src/components/pageFooter/pageFooter.jsx
+++ src/components/pageFooter/pageFooter.jsx
@@ -2,12 +2,6 @@ import './pageFooter.css'
 
 export default function PageFooter() {
   return (
-    <div className="page-footer">
-      <div className="page-footer__blob1" />
-      <div className="page-footer__blob2" />
-      <div className="page-footer__blob3" />
-      <div className="page-footer__blob4" />
-      <div className="page-footer__blob5" />
-    </div>
+    <div className="page-footer" />
   )
 }
